93c73e0d519fc73332afaec6b76aa14af80949b6
unknown <eriksonc_souza@hotmail.com>
2023-05-26 23:21:19 -0300

    upload arquivos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import "./global.css";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Header /> <Routes />
-      </BrowserRouter>
-    </AuthProvider>
+    <div>
+      <Header /> <Register />;
+    </div>
   );
 }
 
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,30 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "../service/api";
 import "./style.css";
 
 function Register() {
+  const [usuario, setUsuario] = useState({});
+
+  function handleChange(e) {
+    setUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(usuario);
+  }
+
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Cadastro de Usuário</h2>
         <div className="row">
           <span>Nome</span>
-          <input type="text" />
+          <input type="text" name="name" onChange={handleChange} />
         </div>
         <div className="row">
           <span>E-mail</span>
-          <input type="text" />
+          <input type="text" name="email" onChange={handleChange} />
         </div>
         <div className="row">
           <span>Username</span>
-          <input type="text" />
+          <input type="text" name="username" onChange={handleChange} />
         </div>
         <div className="row">
           <span>Password</span>
-          <input type="text" />
+          <input type="password" name="password" onChange={handleChange} />
         </div>
         <div className="row">
           <span>Phone</span>
-          <input type="text" />
+          <input type="text" name="phone" onChange={handleChange} />
         </div>
         <div className="row">
           <button>Cadastrar</button>
